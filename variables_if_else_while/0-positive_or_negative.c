4b036c58a24d79aa4bec8e8c08299f847b9a17d5
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-01 12:26:23 +0000

    Add 0-positive_or_negative.c file

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 int main(void)
 {
-    int n;
-
-    srand(time(NULL));
-    n = rand() - RAND_MAX / 2;  /* n can be negative and positive */
-
-    if(n > 0){
-        printf("%d is positive\n", n);
-    }
-    else if(n == 0){
-        printf("%d is zero\n", n);
-    }
-    else{
-        printf("%d is negative\n", n);
-    }
-    
-    return 0;
+int n;
+srand(time(NULL));
+n = rand() - RAND_MAX / 2;  /* n can be negative and positive */
+if(n > 0){
+printf("%d is positive\n", n);
+}
+else if(n == 0){
+printf("%d is zero\n", n);
+}
+else{
+printf("%d is negative\n", n);
+} 
+return 0;
 }
 
