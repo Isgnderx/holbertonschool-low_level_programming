25e3a18ee1a51df313f6e97f763666c136785af8
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-01 11:59:36 +0000

    Add 0-positive_or_negative.c file

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	if(n > 0){
+		printf(n, "is positive\n");
+	}
+	if else(n == 0){
+		printf(n, "is zero\n");
+	}
+	else{
+		printf(n, "is negative\n");
+	}
+return(0);
+}
