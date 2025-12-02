6cca06cad7522b73a2c634d632eceffa9a3923f4
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-02 09:20:54 +0400

    Add 5-print_numbers.c file

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/**
- * main - This program prints all single digit numbers 
- * of base 10 starting from 0, followed by a new line.
- * 
- * Return: Always 0
- */ 
 
- int main(void)
- {
-    int a;
-    for (a = 0; a <= 9; a++)
-    {
-    putchar(a + '0');
-    }
-    putchar('\n');
-    return(0);
- }
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+	}
+
+	putchar('\n');
+
+	return 0;
+}
