269fcd58bb0c41cdbe19a9938d0dd072b3740611
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-02 08:26:51 +0400

    Add 3-print_alphabets.c file

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,16 @@
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	putchar(c);
+putchar(c);
 	}
 
     for (c = 'A'; c <= 'Z'; c++)
 	{
-	putchar(c);
+putchar(c);
     }
-    putchar('\n');
+putchar('\n');
     return (0);
 }
