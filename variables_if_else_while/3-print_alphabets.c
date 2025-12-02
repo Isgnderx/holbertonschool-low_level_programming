2c4f6387ea3ce29f5ce918f9b28ee8e25fc37bc9
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-02 08:21:38 +0400

    Add 3-print_alphabets.c file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+    for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
