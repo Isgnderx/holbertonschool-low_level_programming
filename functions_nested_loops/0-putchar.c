f4c3442f87388ea7845085c1ecbef9b1d0b5882d
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-03 09:17:24 +0400

    Add 0-putchar.c file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- *  main - prints _putchar, followed by a new line.
- * 
- *  Return: Always 0
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error
  */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
- int main(void)
- {
-	printf("_putchar\n");
-	return(0);
- }
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
+}
