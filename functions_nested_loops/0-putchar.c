69d6a9270e03a0b82ce7878fde7edf6374437303
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-03 09:36:37 +0400

    Add main.h file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to stdout
- * @c: The character to print
- *
- * Return: 1 on success, -1 on error
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
+#include "main.h"
 
 /**
  * main - prints _putchar followed by a new line
@@ -27,5 +16,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-    return 0;
+    return (0);
 }
