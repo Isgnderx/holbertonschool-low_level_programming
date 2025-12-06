82e69f54f4751a48e140e06e8c5f84f1c3f3e560
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-06 18:44:35 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,37 +7,36 @@
  */
 void times_table(void)
 {
-    int row, col, result;
+	int row, col, result;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            result = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			result = row * col;
 
-            /* Print the first number in each row (no leading comma) */
-            if (col == 0)
-            {
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (col == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (result < 10)
-                {
-                    _putchar(' ');
-                    _putchar(result + '0');
-                }
-                else
-                {
-                    _putchar((result / 10) + '0');
-                    _putchar((result % 10) + '0');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
 
