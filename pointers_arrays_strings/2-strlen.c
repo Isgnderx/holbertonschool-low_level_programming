cfaf1fa15922a878ccce74781781aa5ccaa42690
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-08 14:01:00 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+
+    int len = 0;
+    
+    while(*(s) != '\0')
+    {
+        len++;
+        s++;
+    }
+
+    return len;
+}
+
