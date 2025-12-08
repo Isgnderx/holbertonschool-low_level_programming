b4c9e1f21cbbc661e5483edb536e74217f5592e3
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-08 14:04:31 +0000

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -8,17 +8,17 @@
  * Return: Always 0.
  */
 
-int _strlen(char *s)
+int _strlen (char *s)
 {
 
     int len = 0;
 
-    while(*(s) != '\0')
+    while (*(s) != '\0')
     {
         len++;
         s++;
     }
 
-    return len;
+    return(len);
 }
 
