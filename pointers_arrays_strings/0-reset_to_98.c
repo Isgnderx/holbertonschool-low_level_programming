ad89a2d450d6f7547bd51c19475f1c6c54e5f0f4
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-08 13:30:14 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - function that takes a pointer to an int
+ * reset_to_98 - function that takes a pointer to an int
  * as parameter and updates the value it points to
+ * @a: salam
  *
  * Return: Always 0.
  */
