2369b5b0f4c152f6f54960471cad2b7b39e95834
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-09 18:16:18 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: input string
+ * _atoi - convert a string to an integer
+ * @s: string to convert
  *
- * Return: the integer converted from the string
+ * Return: the integer value
  */
 int _atoi(char *s)
 {
@@ -12,24 +12,46 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int started = 0;
+	int digit;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] == '+')
-			; /* do nothing */
+			sign = -sign;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
-			result = result * 10 + (s[i] - '0');
+
+			digit = s[i] - '0';
+
+			/* BUILD AS NEGATIVE TO AVOID OVERFLOW */
+			if (sign == 1)
+			{
+				if (result < -214748364)  /* would overflow if made positive later */
+					return (2147483647);   /* INT_MAX */
+				if (result == -214748364 && digit > 7)
+					return (2147483647);   /* INT_MAX */
+			}
+			else
+			{
+				if (result < -214748364)
+					return (-2147483648);  /* INT_MIN */
+				if (result == -214748364 && digit > 8)
+					return (-2147483648);  /* INT_MIN */
+			}
+
+			result = result * 10 - digit;
 		}
 		else if (started)
 			break;
 
-        i++;
+		i++;
 	}
 
-	return (result * sign);
+	/* Convert to positive only if needed */
+	if (sign == 1)
+		result = -result;
+
+	return (result);
 }
 
