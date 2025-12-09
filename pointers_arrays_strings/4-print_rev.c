28425b44e74b5feb57c932fb585c867fbf6abf8e
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-09 05:09:27 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - writes the string in the reverse
+ * @s: I do not fear computers.
+ * I fear the lack of them - Isaac Asimov
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+
+	while (s[len] != '\0')
+		len++;
+
+	for (len = len -1; len >= 0; len--)
+		_putchar(s{len});
+
+	_putcar('\n')
+}
