c5ecc2cd53d0e304404c682f0b6c76980109f94f
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-12 16:28:04 +0000

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,20 +3,20 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to the memory area
- * @b: constant byte to fill with
+ * @b: constant byte to fill
  * @n: number of bytes to fill
  *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
 
-	return (s);
+    return (s);
 }
 
