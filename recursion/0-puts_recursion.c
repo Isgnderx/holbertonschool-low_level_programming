1fa147606cc7274ed2cfa7e7bafbe2ce5608a943
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-16 18:34:32 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _puts_recursion - prints a string followed by a new line
+ * _puts_recursion - Prints a string followed by a new line
  * @s: Pointer to the string to print
  *
  */
