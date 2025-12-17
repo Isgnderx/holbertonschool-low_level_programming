8d2c43ba15d00bbf7fadaef182de91c48c45f08d
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-17 17:36:44 +0000

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: base number
  * @y: power number
@@ -15,6 +15,5 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 
-return(x * _pow_recursion(x, y - 1 ));
+	return (x * _pow_recursion(x, y - 1));
 }
-
