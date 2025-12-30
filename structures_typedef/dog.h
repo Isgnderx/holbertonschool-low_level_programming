9c9a798b08a35d5a4c38b4cff110ec0af7c60244
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-30 18:42:57 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,8 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -21,3 +23,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
+
