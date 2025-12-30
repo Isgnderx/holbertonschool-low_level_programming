93caf8aaa07f721b8cc0d55f423802f0f681de57
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-30 18:55:54 +0000

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a callback function
+ * @name: name to print
+ * @f: pointer to function that prints a string
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
