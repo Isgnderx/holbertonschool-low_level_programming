5a22843cd6f54ea0da05bec2591a8018e8200caf
Isgnderx <ayxanisgenderzade999@gmail.com>
2025-12-30 18:59:06 +0000

    function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-int _putchar(char c);
+#include <stddef.h>  /* size_t, NULL */
+
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+/* Example prototype using size_t */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif /* FUNCTION_POINTERS_H */
