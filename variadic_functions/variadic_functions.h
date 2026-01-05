b3b66a0f86ed883471a9f7ce67ebd318d9df9ae4
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-05 11:36:33 +0000

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+#include <stdarg.h>
 
-#endif
+/* Struct for mapping format char to function */
+typedef struct printer
+{
+	char c;
+	void (*f)(va_list);
+} printer_t;
+
+/* Function prototypes */
+void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+#endif /* VARIADIC_FUNCTIONS_H */
 
