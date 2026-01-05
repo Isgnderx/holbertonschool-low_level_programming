b59065eb02e22601657c287dbb39c045b78720d5
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-05 11:29:52 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,39 +11,53 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *str;
+	unsigned int i = 0, j;
 	char *sep = "";
-	char c;
-	int num;
-	double f;
+	char *s;
+
+	typedef struct printer
+	{
+		char c;
+		void (*f)(va_list);
+	} printer_t;
+
+	void print_char(va_list a) { printf("%c", va_arg(a, int)); }
+	void print_int(va_list a) { printf("%d", va_arg(a, int)); }
+	void print_float(va_list a) { printf("%f", va_arg(a, double)); }
+	void print_string(va_list a)
+	{
+		s = va_arg(a, char *);
+		printf("%s", s ? s : "(nil)");
+	}
+
+	printer_t p[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		while (format[i] != 'c' && format[i] != 'i' &&
-		       format[i] != 'f' && format[i] != 's')
-			i++;
-
-		printf("%s", sep);
-		sep = ", ";
-
-		if (format[i] == 'c')
-			c = va_arg(args, int), printf("%c", c);
-		if (format[i] == 'i')
-			num = va_arg(args, int), printf("%d", num);
-		if (format[i] == 'f')
-			f = va_arg(args, double), printf("%f", f);
-		if (format[i] == 's')
-			str = va_arg(args, char *),
-			printf("%s", str ? str : "(nil)");
-
+		j = 0;
+		while (p[j].c)
+		{
+			if (format[i] == p[j].c)
+			{
+				printf("%s", sep);
+				p[j].f(args);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
 		i++;
 	}
 
 	va_end(args);
-
 	printf("\n");
 }
 
