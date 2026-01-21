feb981eeb80fca6ef85203445fcbeec1140c8934
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-21 14:40:27 +0400

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,1 +1,60 @@
-include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: Actual number of letters read and printed, 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
+
+    /* Check for NULL filename */
+    if (filename == NULL)
+        return (0);
+
+    /* Allocate memory for buffer */
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    /* Open the file */
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    /* Read from the file */
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    /* Write to standard output */
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    /* Clean up and return */
+    free(buffer);
+    close(fd);
+    return (bytes_written);
+}
+
