da16b696892d13fbae41a55a10bdab91ce1a5799
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-21 14:46:09 +0400

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,56 +5,60 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX standard output
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: Actual number of letters read and printed, 0 on failure
+ * Return: actual number of letters it could read and print
+ *         if the file can not be opened or read, return 0
+ *         if filename is NULL return 0
+ *         if write fails or does not write expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
 
-    /* Check for NULL filename */
-    if (filename == NULL)
-        return (0);
+	/* Check if filename is NULL */
+	if (filename == NULL)
+		return (0);
 
-    /* Allocate memory for buffer */
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	/* Allocate memory for buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    /* Open the file */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	/* Open the file */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    /* Read from the file */
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	/* Read from the file */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    /* Write to standard output */
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	/* Write to standard output */
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    /* Clean up and return */
-    free(buffer);
-    close(fd);
-    return (bytes_written);
+	/* Clean up resources */
+	free(buffer);
+	close(fd);
+
+	/* Return number of letters printed */
+	return (n_written);
 }
-
