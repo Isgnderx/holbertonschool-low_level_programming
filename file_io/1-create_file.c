9885d69ec7a44da67af4e5485ce27ae74613ce26
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-25 14:43:16 +0400

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,47 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #include "main.h"
 
 /**
- * create_file - creates a file with given text content
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to file
+ * create_file - Creates a file.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL terminated string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 otherwise.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes_written = 0;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int f, w, ln = 0;
 
-	/* Check if filename is NULL */
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
-	/* Open file with O_CREAT, O_WRONLY, O_TRUNC flags */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
-	if (fd == -1)
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (f == -1)
+	{
 		return (-1);
+	}
 
-	/* Write text_content if it's not NULL */
 	if (text_content != NULL)
 	{
-		/* Calculate length of text_content */
-		while (text_content[bytes_written] != '\0')
-			bytes_written++;
-
-		/* Write to file */
-		if (write(fd, text_content, bytes_written) == -1)
+		while (text_content[ln] != '\0')
 		{
-			close(fd);
+			++ln;
+		}
+
+		w = write(f, text_content, ln);
+
+		if (w == -1)
+		{
+			close(f);
 			return (-1);
 		}
 	}
 
-	close(fd);
+	close(f);
 	return (1);
 }
